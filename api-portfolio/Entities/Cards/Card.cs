8e87a1fc8bc6cac24bff6a4729d178c106ec4c58
Felix Kevin Acarapi Rivero <kevin.acarapi@itscordoba.edu.ar>
2023-10-31 13:04:49 -0300

    entidad card creada

---
+++ api-portfolio/Entities/Cards/Card.cs
@@ -0,0 +1,17 @@
+namespace api_portafolio.Entities.Cards;
+
+public class Card 
+{
+    private long id = 0;
+    public long Id 
+    { 
+        get
+        {
+            return this.id;
+        } 
+        set
+        {
+            this.id = value;
+        }
+    }
+}
