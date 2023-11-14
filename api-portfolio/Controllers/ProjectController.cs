75087f553dd5acc196852d2a44fd9fc847445fb4
matias <matias.amado@itscordoba.edu.ar>
2023-11-14 10:06:40 -0500

    avance con project controller con el metodo put - continuar

--- api-portfolio/Controllers/ProjectController.cs
+++ api-portfolio/Controllers/ProjectController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using api_portafolio.Entities.Projects;
 using System.Diagnostics.CodeAnalysis;
-using api_portafolio.DTO.SoftSkill;
 using api_portafolio.DTO.ProjectDTO;
 using api_portafolio.Entities.Skills.TechnicalSkills;
 using api_portafolio.Entities.TechnologiesCatalog;
@@ -52,5 +51,43 @@ public class ProjectController : ControllerBase
             });
         }
         return Ok(projectResponseDTOs);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Project>> Post([FromBody] Project project)
+    {
+        if (this.dataContext != null && this.dataContext.Projects != null)
+        {
+            await this.dataContext.Projects.AddAsync(project);
+
+            await this.dataContext.SaveChangesAsync();
+        }
+        return Ok(project);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Project>> Put(
+        [FromRoute] long id,
+        [FromBody] Project project)
+    {
+        if (this.dataContext != null && this.dataContext.Projects != null)
+        {
+            User user =await this.dataContext.Users
+            .Include(user => user.Projects)
+            .ThenInclude(project => project.TechnologiesByProject)
+            .ThenInclude(technologyByProject => technologyByProject.Technology)
+            .Where(user => user.Id == id)
+            .FirstOrDefaultAsync();
+
+            if(user == null){
+                return NotFound("Usuario no encontrado")
+            }
+
+             Project proj = user.Projects.FirstOrDefault(proj => p.Id == project.Id);
+
+            await this.dataContext.SaveChangesAsync();
+        }
+
+        return Ok(project);
     }
 }
